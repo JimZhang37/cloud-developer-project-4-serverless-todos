89f93afaa4ae0b7454c7101011249519d6fdfcce
ZHANG Yaohua <zhangyaohua40605@gmail.com>
2019-10-11 13:35:22 +0800

    third commit, with all files required by the second commit

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,38 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import * as AWS  from 'aws-sdk'
+import * as uuid from 'uuid'
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  const parsedBody: CreateTodoRequest = JSON.parse(event.body)
+  console.log('parsedBody from CreateTodo request', parsedBody)
+  const itemId = uuid.v4()
+
+  const newItem = {
+    todoId: itemId,
+    ...parsedBody
+  }
+
+  console.log('new Item for createTodo', newItem)
+
+  await docClient.put({
+    TableName: todosTable,
+    Item: newItem
+  }).promise()
 
   // TODO: Implement creating a new TODO item
-  return undefined
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      newItem
+    })
+  }
 }
