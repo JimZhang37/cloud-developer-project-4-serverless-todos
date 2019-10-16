c1bee86615edadb104a7d4f6bf9b55662871e2a7
ZHANG Yaohua <zhangyaohua40605@gmail.com>
2019-10-16 21:52:22 +0800

    all api are runing, 3 problems remain

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,42 @@
 import 'source-map-support/register'
-
+import * as AWS  from 'aws-sdk'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+})
+
+const bucketName = process.env.IMAGES_S3_BUCKET
+//const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   //const todoId = event.pathParameters.todoId
   console.log("event is", event)
   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
+  console.log('Caller event', event)
+  const todoId = event.pathParameters.todoId
+
+  const url = getUploadUrl(todoId)
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+
+      uploadUrl: url
+    })
+  }
+
   return undefined
+}
+
+
+function getUploadUrl(imageId: string) {
+  return s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: imageId,
+    Expires: 300//hardcode 300 here because the parameter from environment is interpreted as string, while a number is expected
+  })
 }
---
+++ backend/src/lambda/http/updateTodo.ts
@@ -0,0 +1,48 @@
+import 'source-map-support/register'
+import * as AWS  from 'aws-sdk'
+import {parseUserId} from '../auth/utils'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const todosTable = process.env.TODOS_TABLE
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  //const todoId = event.pathParameters.todoId
+  //const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+  console.log("event is", event)
+  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
+  const parsedBody: UpdateTodoRequest = JSON.parse(event.body)
+  const todoId = event.pathParameters.todoId
+
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+  const uId = parseUserId(jwtToken)
+
+  const item = {
+    userId: uId,
+    todoId: todoId
+  }
+
+  const result = await docClient.update({
+    TableName: todosTable,
+    Key: item,
+    UpdateExpression: "set dueDate = :dueDate, done = :done",
+    ExpressionAttributeValues: {
+
+        ":dueDate": parsedBody.dueDate,
+        ":done": parsedBody.done
+    }
+  }).promise()
+
+  console.log('the result of update is ', result)
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: ""
+  }
+}
